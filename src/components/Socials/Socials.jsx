bf41ab23e6cc9992e08de39cd27efc2edf83d691
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-04-09 12:24:30 -0600

    Update socials.jsx

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -4,7 +4,7 @@ import SocialIcon from "./SocialIcon";
 const socialData = [
     { icon: <i className="fa-brands fa-square-github"></i>, link: "https://github.com/jessicaArvizu" },
     { icon: <i className="fa-brands fa-linkedin"></i>, link: "https://www.linkedin.com/in/jessica-arvizu/" },
-    { icon: <i className="fa-brands fa-codepen"></i>, link: "https://codepen.io/Jessica-Arvizu" },
+    { icon: <i className="fa-brands fa-codepen"></i>, link: "https://codesandbox.io/u/jessicaarvizu" },
     { icon: <i className="fa-solid fa-code"></i>, link: "https://www.frontendmentor.io/profile/jessicaArvizu" },
     { icon: <i className="fa-solid fa-graduation-cap"></i>, link: "https://www.codecademy.com/profiles/jessArvizu" }
 ];
