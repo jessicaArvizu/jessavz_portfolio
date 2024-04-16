fc5069b6d31af55a44e2c93652698a2db4f71933
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-04-16 13:29:34 -0600

    Update SkillItem.jsx

--- src/components/Skills/SkillItem.jsx
+++ src/components/Skills/SkillItem.jsx
@@ -5,7 +5,7 @@ export default function SkillItem(props) {
         <div className="text-center mb-2">
             <i className={`${props.iconClass} text-5xl mb-2 text-cotton`}></i>
             <br/>
-            <span className="text-s text-amethyst">{props.name}</span>
+            <span className="text-s text-peach">{props.name}</span>
         </div>
     );
 }
