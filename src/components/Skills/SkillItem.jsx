d2c5ba5baafcf18df4c892d73aaabe27ee239032
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-04-16 12:51:07 -0600

    Update SkillItem.jsx

--- src/components/Skills/SkillItem.jsx
+++ src/components/Skills/SkillItem.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function SkillItem(props) {
     return (
         <div className="text-center mb-2">
-            <i className={`${props.iconClass} text-5xl mb-2 text-peach`}></i>
+            <i className={`${props.iconClass} text-5xl mb-2 text-cotton`}></i>
             <br/>
-            <span>{props.name}</span>
+            <span className="text-s text-amethyst">{props.name}</span>
         </div>
     );
 }
