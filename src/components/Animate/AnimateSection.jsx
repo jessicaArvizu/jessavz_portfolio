6a4ebc7db96b0c5fda512e00206515738f3c28e5
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-05-03 20:13:11 -0600

    Small chnges

--- src/components/Animate/AnimateSection.jsx
+++ src/components/Animate/AnimateSection.jsx
@@ -11,7 +11,7 @@ function AnimateSection({ children }) {
                 style={{
                     transform: isInView ? "none" : "translateX(-200px)",
                     opacity: isInView ? 1 : 0,
-                    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.9s"
                 }}
             >
                 {children}
